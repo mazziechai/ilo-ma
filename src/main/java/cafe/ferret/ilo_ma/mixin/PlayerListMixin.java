db8052b379456799d87e49c68d6a26c59e7af9ab
l4ssc <lassc.alex@protonmail.com>
2022-07-07 00:03:49 -0500

    Update mixin, actually send the player a message when cancelling for non-toki pona contents

--- src/main/java/cafe/ferret/ilo_ma/mixin/PlayerListMixin.java
+++ src/main/java/cafe/ferret/ilo_ma/mixin/PlayerListMixin.java
@@ -1,11 +1,15 @@
 package cafe.ferret.ilo_ma.mixin;
 
 import cafe.ferret.ilo_ma.tpt.TokiPonaTasoValidator;
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.ChatSender;
 import net.minecraft.network.chat.ChatType;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.PlayerChatMessage;
+import net.minecraft.network.chat.contents.LiteralContents;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.FilteredText;
 import net.minecraft.server.players.PlayerList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,16 +19,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.function.Function;
 
 @Mixin(PlayerList.class)
-public class PlayerListMixin {
+public abstract class PlayerListMixin {
 	/**
 	 *
 	 */
-	@Inject(method = "broadcastChatMessage(Lnet/minecraft/network/chat/PlayerChatMessage;Ljava/util/function/Function;Lnet/minecraft/network/chat/ChatSender;Lnet/minecraft/resources/ResourceKey;)V", at = @At("HEAD"), cancellable = true)
-	public void onBroadcastChatMessage(PlayerChatMessage playerChatMessage, Function<ServerPlayer, PlayerChatMessage> function, ChatSender chatSender, ResourceKey<ChatType> resourceKey, CallbackInfo ci) {
-		String content = playerChatMessage.serverContent().getString();
+	@Inject(method = "broadcastChatMessage(Lnet/minecraft/server/network/FilteredText;Lnet/minecraft/server/level/ServerPlayer;Lnet/minecraft/resources/ResourceKey;)V", at = @At("HEAD"), cancellable = true)
+	public void onBroadcastChatMessage(FilteredText<PlayerChatMessage> filteredText, ServerPlayer serverPlayer, ResourceKey<ChatType> resourceKey, CallbackInfo ci) {
+		String content = filteredText.raw().serverContent().getString();
 
-		if (!content.startsWith("/") && chatSender != null) {
+		if (!content.startsWith("/") && serverPlayer != null) {
 			if (!TokiPonaTasoValidator.validateMessage(content)) {
+				serverPlayer.displayClientMessage(Component.literal("o toki Inli ala lon ma ni a!").withStyle(ChatFormatting.RED), false);
 				ci.cancel();
 			}
 		}
