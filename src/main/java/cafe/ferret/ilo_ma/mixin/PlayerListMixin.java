9aa45999bc64a14be44d2878a24f6750bb7b2a2f
Mazzie <mazziechai@gmail.com>
2022-07-06 18:09:21 -0700

    implement basic toki pona taso

--- src/main/java/cafe/ferret/ilo_ma/mixin/PlayerListMixin.java
+++ src/main/java/cafe/ferret/ilo_ma/mixin/PlayerListMixin.java
@@ -1,7 +1,10 @@
 package cafe.ferret.ilo_ma.mixin;
 
+import cafe.ferret.ilo_ma.tpt.TokiPonaTasoValidator;
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.ChatSender;
 import net.minecraft.network.chat.ChatType;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.PlayerChatMessage;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerPlayer;
@@ -11,15 +14,23 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 
 @Mixin(PlayerList.class)
 public class PlayerListMixin {
+	/**
+	 *
+	 */
 	@Inject(method = "broadcastChatMessage(Lnet/minecraft/network/chat/PlayerChatMessage;Ljava/util/function/Function;Lnet/minecraft/network/chat/ChatSender;Lnet/minecraft/resources/ResourceKey;)V", at = @At("HEAD"), cancellable = true)
 	public void onBroadcastChatMessage(PlayerChatMessage playerChatMessage, Function<ServerPlayer, PlayerChatMessage> function, ChatSender chatSender, ResourceKey<ChatType> resourceKey, CallbackInfo ci) {
-		if (!playerChatMessage.serverContent().getString().startsWith("/") && chatSender != null) {
-			StringBuilder sb = new StringBuilder(playerChatMessage.serverContent().getString());
-			System.out.println(playerChatMessage.serverContent().getString());
+		String content = playerChatMessage.serverContent().getString();
+
+		if (!content.startsWith("/") && chatSender != null) {
+			if (!TokiPonaTasoValidator.validateMessage(content)) {
+				ci.cancel();
+			}
 		}
 	}
 }
---
+++ src/main/java/cafe/ferret/ilo_ma/tpt/TokiPonaTasoValidator.java
@@ -0,0 +1,40 @@
+package cafe.ferret.ilo_ma.tpt;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TokiPonaTasoValidator {
+	private static final Pattern PATTERN = Pattern.compile("^(?:[k-nps]?[aeiou]|[jt][aeou]|w[aei])(?:(?:n?[klps]|[mn])[aeiou]|n?[jt][aeou]|n?w[aei])*n?$", Pattern.CASE_INSENSITIVE);
+
+	/**
+	 * Validates a string against a toki pona taso regex.
+	 * @param content The string to validate.
+	 * @return True if the string passed the regex.
+	 */
+	public static boolean validateMessage(String content) {
+		// Split each word into its own string
+		List<String> wordsWithSymbols = Arrays.stream(content.split(" ")).toList();
+
+		// We don't want to count words that contain only symbols (such as emoticons)
+		List<String> alphabeticWords = new ArrayList<>();
+		for (String word : wordsWithSymbols) {
+			if (word.matches("^[a-zA-Z]*?")) {
+				alphabeticWords.add(word);
+			}
+		}
+
+		// Check each word with the pattern
+		for (String word : alphabeticWords) {
+			Matcher matcher = PATTERN.matcher(word);
+
+			if (!matcher.find()) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
